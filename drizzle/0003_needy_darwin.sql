8ae469c4e9b2aa1a925fe22e0ade447e1bb01160
SamnangKong426 <132801830+SamnangKong426@users.noreply.github.com>
2026-09-07 03:18:54 +0000

    Close #43 Create mock data Fixes

--- drizzle/0003_needy_darwin.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE `test3` (
-	`id` text PRIMARY KEY NOT NULL,
-	`name` text NOT NULL
-);
---
+++ src/routes/rank/+server.ts
@@ -0,0 +1,12 @@
+import { users } from "$lib/server/db/schema"
+import { get_db } from "$lib/server/db"
+import { json, type RequestHandler } from "@sveltejs/kit"
+
+export const GET: RequestHandler = async ({ platform }) => {
+    const db = get_db(platform?.env)
+
+    const ranks = await db.select().from(users);
+
+    console.log("ranks", ranks)s
+    return json("hello")
+}
